4092aeb8fda88d1ef1e054d7c84e7dc6b3763298
Vasilisa <gerka3479@mail.ru>
2023-11-06 13:39:09 +0300

    StomatologyProject

--- KP/obj/Debug/MainWindow.g.i.cs
+++ KP/obj/Debug/MainWindow.g.i.cs
@@ -52,7 +52,7 @@ namespace KP {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/KP;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/StomatologyProject;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
